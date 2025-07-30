97f0ae403e4837ba3a684ad68f87bebd24a313b1
Phanindra <phanivushakola@gmail.com>
2025-07-30 22:30:43 +0530

    Deploy Flutter web app

--- tictoktoe/android/app/src/main/kotlin/com/example/tictoktoe/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tictoktoe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- tictoktoe/flutter_bootstrap.js
+++ tictoktoe/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2455849547"
+    serviceWorkerVersion: "3833138128"
   }
 });
